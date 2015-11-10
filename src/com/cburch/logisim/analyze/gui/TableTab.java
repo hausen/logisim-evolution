6557d665ff5659aa66876200abd56560fc4a3bdc
Rodrigo Hausen <hausen@compscinet.org>
2015-11-10 12:11:24 -0200

    habilitando antialiasing

--- src/com/cburch/logisim/analyze/gui/TableTab.java
+++ src/com/cburch/logisim/analyze/gui/TableTab.java
@@ -35,7 +35,9 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Rectangle;
+import java.awt.RenderingHints;
 import java.awt.event.MouseEvent;
 
 import javax.swing.JPanel;
@@ -270,6 +272,14 @@ class TableTab extends JPanel implements TruthTablePanel, TabInterface {
 
 	@Override
 	public void paintComponent(Graphics g) {
+		Graphics2D g2 = (Graphics2D)g;
+		g2.setRenderingHint(
+			RenderingHints.KEY_TEXT_ANTIALIASING,
+			RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+		g2.setRenderingHint(
+			RenderingHints.KEY_ANTIALIASING,
+			RenderingHints.VALUE_ANTIALIAS_ON);
+
 		super.paintComponent(g);
 
 		caret.paintBackground(g);
